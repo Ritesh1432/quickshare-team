b6fe71bf35b2c0bb7b121f18b927105b80cc9139
tatheersayed <tatheer.sayed@lendenclub.com>
2022-07-27 15:46:35 +0530

    test commit

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -3,16 +3,37 @@ import '../css/ChangePassword.css'
 import  {useState} from 'react'
 // import { Navigate } from 'react-router-dom'
 
-export default function ChangePassword() {
+export default function ChangePassword({userDetail}) {
     const [formData,setFormData] = useState([])
 
-
+    const handleChange = ((e) => {
+        e.preventDefault();
+    setFormData({
+        ...formData,
+        [e.target.name] : e.target.value
+        })
+    console.log(formData);
+    })
     
-    const handleSubmit  =((e) => {
-        if(formData.oldpw !== formData.pw)
-        alert('Password did not match')})
+    const handleSubmit  = ((e) => {
+        e.preventDefault();
+        if(formData.oldpw !== userDetail.pw)
+            alert("Incorrect Old Password!")
+        else if(formData.newpw !== formData.cpw)
+            alert("New passwords didn't match")
+        else 
+        {
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "pw":  formData.newpw, "cpw":formData.newpw })
+            };
+            fetch('http://localhost:3000/users', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data.id))
+        }
         
-       
+    });
     
 
 
@@ -36,7 +57,7 @@ export default function ChangePassword() {
                 </div>
                 <div className="blocks1">
                     <div>
-                        <input name='repw' placeholder='Renter new password' type='text' onChange={handleChange}/>
+                        <input name='cpw' placeholder='Confirm new password' type='text' onChange={handleChange}/>
                     </div>
                 
                     <button className='passwordbutton'>Edit</button>
