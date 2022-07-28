235edc38677a98eed9f667bf3033241607ddb292
Ritesh1432 <riteshdewoolkar@gmail.com>
2022-07-28 22:20:51 +0530

    Updated My Profile Component

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,68 +1,91 @@
- import React, { useState } from 'react'
-import '../css/EditProfile.css'
-import ChangePassword from './ChangePassword';
+import React, { useState } from "react";
+import styles from "../css/EditProfile.module.css";
+import ChangePassword from "./ChangePassword";
 
-export default function EditProfile({userDetail}) {
-    const [updatedData, setUpdatedData] = useState(userDetail);
+export default function EditProfile({ userDetail }) {
+  const [updatedData, setUpdatedData] = useState(userDetail);
 
-    const handleChange = ((e) => {
-        setUpdatedData({
-            ...updatedData,
-            [e.target.name] : e.target.value
-            })
-        // console.log(updatedData);
-        })
-
-    const handleSubmit = ((e) => {
-        e.preventDefault();
-        console.log("UPDATED DATA: ", updatedData);
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(updatedData)
-        };
-        fetch('http://localhost:3000/users/'+userDetail.id.toString(), requestOptions)
-            // .then(response => response.json())
-            // .then(data => console.log(data.id));
-        window.location.reload(false);
-
+  const handleChange = (e) => {
+    setUpdatedData({
+      ...updatedData,
+      [e.target.name]: e.target.value,
     });
+    // console.log(updatedData);
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("UPDATED DATA: ", updatedData);
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedData),
+    };
+    fetch(
+      "http://localhost:3000/users/" + userDetail.id.toString(),
+      requestOptions
+    );
+    // .then(response => response.json())
+    // .then(data => console.log(data.id));
+    window.location.reload(false);
+  };
 
   return (
-    <div className="main">
-        <div className="submain">
-            <div className="heading">
-                Edit Your Details
-            </div>
-            <form onSubmit={handleSubmit}>
-                <div className="blocks">
-                    <div>
-                        <input name='fname' placeholder='First name' type='text' onChange={handleChange} value={updatedData.fname}/>
-                    </div>
-                </div>
-                <div className="blocks">
-                    <div>
-                        <input name='lname' placeholder='Last name' type='text' onChange={handleChange} value={updatedData.lname} />
-                    </div>
-                </div>
-                <div className="blocks">
-                    <div>
-                        <input name='email' placeholder='Enter Email ID' type='email' onChange={handleChange} value={updatedData.email}/>
-                    </div>
-                </div>
-                <div className="blocks">
-                    <div>
-                        <input name='pno' placeholder='Enter Phone Number' type='number' onChange={handleChange} value={updatedData.pno} />
-                    </div>
-                </div>
-                <div>
-                    <button className="signup" type='submit' value='editProfile'>Edit</button>
-                </div>
-            </form>
-
-
-            </div>
-        </div>
-  )
+    <div className={styles.main}>
+      <div className={styles.submain}>
+        <div className={styles.heading}>Edit Your Details</div>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.blocksStyles}>
+           
+            <input
+              name="fname"
+              placeholder="First name"
+              type="text"
+              onChange={handleChange}
+              value={updatedData.fname}
+              className={styles.inputClassStyle}
+            />
+          </div>
+          <div className={styles.blocksStyles}>
+          
+            <input
+              name="lname"
+              placeholder="Last name"
+              type="text"
+              onChange={handleChange}
+              value=  {updatedData.lname}
+              className={styles.inputClassStyle}
+            />
+          </div>
+          <div className={styles.blocksStyles}>
+            
+            <input
+              name="email"
+              placeholder="Enter Email ID"
+              type="email"
+              onChange={handleChange}
+              value={updatedData.email}
+              className={styles.inputClassStyle}
+            />
+          </div>
+          <div className={styles.blocksStyles}>
+            
+            <input
+              name="pno"
+              placeholder="Enter Phone Number"
+              type="number"
+              onChange={handleChange}
+              value={updatedData.pno}
+              className={styles.inputClassStyle}
+            />
+          </div>
+          <div>
+            <button className={styles.editProfileButton} type="submit" value="editProfile">
+              Save Changes
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
